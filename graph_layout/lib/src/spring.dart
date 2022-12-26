44ca8e931db54116c45f393fcb2c908ae84a0d0f
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-12-26 01:11:12 +0000

    check whether the layout is stable on each iteration also introduce `iterateUntilStable` method

--- graph_layout/lib/src/spring.dart
+++ graph_layout/lib/src/spring.dart
@@ -22,12 +22,25 @@ class SpringSystem {
     }
   }
 
-  void iterate() {
+  /// Perform one iteration of the spring algorithm, updating [nodeLayout].
+  ///
+  /// Returns [true] if running one iteration does not change the position of
+  /// each node by much in each axis.
+  bool iterate() {
+    // TODO: Make these constants named parameters with default values?
+
+    // TODO: Tune these constants.
     const c1 = 8;
     const c2 = 0.6;
     const c3 = 1;
     const c3Wall = 0.5;
     const c4 = 0.01;
+
+    // The threshold for a small change in node position in one axis.
+    const stableThreshold = 0.001;
+
+    // Initially assume this layout is stable.
+    var isStable = true;
 
     // Calculate the forces on each node in the graph.
     for (final node in adjacencyList.keys) {
@@ -60,20 +73,37 @@ class SpringSystem {
       forceOnThisNode += Vector2(1 - x, 0).scaled(c3Wall / sqrt(x));
 
       nodeLayout.update(node, (position) {
-        var newPosition = position + forceOnThisNode.scaled(c4);
+        final positionChange = forceOnThisNode.scaled(c4);
+        var newPosition = position + positionChange;
 
         // If a node will be outside the unit square as a result of the forces
-        // applied to it this iteration, randomise its position instead.
+        // applied to it this iteration, randomise its position instead. The new
+        // position is guaranteed to lie inside the unit square.
         if (newPosition.x >= 1 ||
             newPosition.x <= 0 ||
             newPosition.y >= 1 ||
             newPosition.y <= 0) {
           newPosition = _randomVector2();
+          isStable = false;
         }
 
-        // The new position is guaranteed to lie inside the unit square.
+        // If the position of this node changes too much, the layout is not
+        // stable.
+        if (isStable &&
+            max(positionChange.x.abs(), positionChange.y.abs()) >
+                stableThreshold) {
+          isStable = false;
+        }
+
         return newPosition;
       });
     }
+
+    return isStable;
+  }
+
+  /// Repeatedly run [iterate] until a stable layout is obtained.
+  void iterateUntilStable() {
+    while (!iterate()) {}
   }
 }
