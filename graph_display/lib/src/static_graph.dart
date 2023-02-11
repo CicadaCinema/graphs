b5af21f48e5887e61303555e2c97d64c5f5861c8
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-02-11 20:14:57 +0000

    wait until the resizing operation has stopped before re-computing the static layout

--- graph_display/lib/src/static_graph.dart
+++ graph_display/lib/src/static_graph.dart
@@ -1,3 +1,4 @@
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:graph_layout/graph_layout.dart';
 
@@ -7,11 +8,13 @@ import 'graph_theme.dart';
 class StaticGraph extends StatefulWidget {
   final StaticLayoutAlgorithm layoutAlgorithm;
   final GraphThemePreferences themePreferences;
+  final Duration resizeBufferDuration;
 
   const StaticGraph({
     Key? key,
     required this.layoutAlgorithm,
     this.themePreferences = const GraphThemePreferences(),
+    this.resizeBufferDuration = const Duration(milliseconds: 100),
   }) : super(key: key);
 
   @override
@@ -19,19 +22,53 @@ class StaticGraph extends StatefulWidget {
 }
 
 class _StaticGraphState extends State<StaticGraph> {
+  late RestartableTimer _redrawTimer;
+
+  double? _previousWidth;
+  double? _previousHeight;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initially cancel the timer, so that it can only be triggered after a call
+    // to [widget.layoutAlgorithm.updateLayoutParameters].
+    _redrawTimer = RestartableTimer(
+      widget.resizeBufferDuration,
+      () {
+        setState(() {
+          // TODO: compute this in an isolate or a web worker, depending on platform
+          widget.layoutAlgorithm.computeLayout();
+        });
+      },
+    );
+    _redrawTimer.cancel();
+  }
+
+  @override
+  void dispose() {
+    _redrawTimer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-          widget.layoutAlgorithm.updateLayoutParameters(
-            width: constraints.maxWidth,
-            height: constraints.maxHeight,
-            nodeRadius: widget.themePreferences.nodeRadius,
-          );
+          // The widget has been resized.
+          if (constraints.maxWidth != _previousWidth ||
+              constraints.maxHeight != _previousHeight) {
+            widget.layoutAlgorithm.updateLayoutParameters(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight,
+              nodeRadius: widget.themePreferences.nodeRadius,
+            );
+            _redrawTimer.reset();
 
-          // TODO: Wait a while until the resizing operation stops, then call this potentially-expensive operation in an isolate while displaying a loading indicator.
-          widget.layoutAlgorithm.computeLayout();
+            // Update the cached dimensions.
+            _previousWidth = constraints.maxWidth;
+            _previousHeight = constraints.maxHeight;
+          }
 
           return CustomPaint(
             painter: GraphPainter(
