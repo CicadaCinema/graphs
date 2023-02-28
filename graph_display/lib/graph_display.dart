6e2d6aa2b5366daf88554a3fec3d2d42baa3d314
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-02-28 13:15:03 +0000

    export the entirety of `package:graph_layout` from `package:graph_display`

--- graph_display/lib/graph_display.dart
+++ graph_display/lib/graph_display.dart
@@ -3,3 +3,5 @@ library graph_display;
 export 'src/graph_theme.dart' show GraphThemePreferences;
 export 'src/interactive_graph.dart';
 export 'src/static_graph.dart';
+
+export 'package:graph_layout/graph_layout.dart';
