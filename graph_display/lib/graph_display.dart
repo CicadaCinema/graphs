0ee84d9536f0e09c9d7581786c7763ed7f58d4c6
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-03-01 20:23:49 +0000

    appease the formatter

--- graph_display/lib/graph_display.dart
+++ graph_display/lib/graph_display.dart
@@ -3,4 +3,3 @@ library graph_display;
 export 'src/graph_theme.dart' show GraphThemePreferences;
 export 'src/interactive_graph.dart';
 export 'src/static_graph.dart';
-
--- graph_display/lib/graph_layout.dart
+++ graph_display/lib/graph_layout.dart
@@ -1,4 +1,3 @@
 library graph_layout;
 
 export 'package:graph_layout/graph_layout.dart';
-
