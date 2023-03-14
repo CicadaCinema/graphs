68f3a93727e168c5b6a91a9f7fa02503907baf90
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2023-03-14 10:04:25 +0000

    export typedefs from their new location in `typedefs.dart`

--- graph_layout/lib/graph_layout.dart
+++ graph_layout/lib/graph_layout.dart
@@ -1,6 +1,7 @@
 library graph_layout;
 
 export 'src/data_structures/data_structures.dart';
+export 'src/data_structures/typedefs.dart';
 export 'src/layout_algorithms/layout_algorithms.dart';
 export 'src/layout_algorithms/interactive/eades.dart';
 export 'src/layout_algorithms/static/fruchterman_reingold.dart';
